db32de171afebd199a4ffe2209e2786b340163b8
Claire Bromley <clairelbromley@gmail.com>
2018-07-11 22:52:28 +0900

    first commit of cellularization code, including starting work on exponential fit to kymograph edge

---
+++ cellularization.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 18:04:16 2018
+
+@author: yuchiunwang
+"""
+
+from skimage import io, filters
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import scipy.optimize as opt
+
+def fitted_func(t, a, b, tau):
+	return (a - b * exp(-t / tau))
+
+def derivative_fitted_func(t, b, tau):
+	return ( (b / tau) * exp(-t/ tau))
+
+Folder = '20150601_01_sqhGFP'
+
+path = '/Users/cib/Documents/CF formation imaging/Exp27/E3TL/' + Folder
+tif_filename = path + '/cellu_front_y_t.tif'
+txt_filename = path + '/time_pixel_scales.txt'
+DF_txt = pd.read_csv(txt_filename, sep=' ')
+type(DF_txt)
+t_step_size = '%.1f'%(DF_txt.iloc[0,0])
+pixel_size = '%.3f'%(DF_txt.iloc[0,1])
+
+tif = io.imread(tif_filename)
+type(tif)
+
+[rows, cols] = np.shape(tif)
+# plt.figure()
+# plt.imshow(tif[:,:], cmap="gray")
+        
+DF = pd.DataFrame(tif)
+print(DF.columns)
+Y = len(DF)
+X = len(DF.columns)
+cdn = []
+
+""" Same as below
+for i, y in enumerate(np.arange(0, Y, 1)):
+   for j, x in enumerate(np.arange(0, X, 1)):
+       if DF.iloc[i, j] == 255:
+           edge = (i, j)
+           cdn.append(edge)
+DF_edge = pd.DataFrame(cdn)       
+"""   
+
+for y in np.arange(0, Y, 1):
+   for x in np.arange(0, X, 1):
+       if DF.iloc[y, x] == 255:
+           edge = (y, x)
+           cdn.append(edge)
+DF_edge = pd.DataFrame(cdn)    
+
+DF_edge = DF_edge.rename(columns = {0:'time', 1: 'position'})
+pos_max = DF_edge['position'].max(axis = 0)
+time_pos_max = DF_edge[DF_edge['position'] == pos_max]
+time_end = time_pos_max['time'].max(axis = 0)
+DF_edge_max = DF_edge[DF_edge['time'] <= time_end]
+
+plt.figure()
+plt.subplot(321)
+ax1 = plt.plot(DF_edge['time'],DF_edge['position'], 'X')
+ax2 = plt.plot(DF_edge_max['time'],DF_edge_max['position'], '.')
+
+plt.subplot(322)
+z = np.polyfit(DF_edge_max['time'], DF_edge_max['position'], 1)
+print(z)
+p = np.poly1d(z)
+print(p(10))
+xp = np.linspace(0, time_end, 1000)
+ax = plt.plot(DF_edge_max['time'],DF_edge_max['position'], '.', xp, p(xp), '-')
+
+plt.subplot(323)
+z = np.polyfit(DF_edge_max['time'], DF_edge_max['position'], 2)
+print(z)
+p = np.poly1d(z)
+print(p(10))
+xp = np.linspace(0, time_end, 1000)
+ax = plt.plot(DF_edge_max['time'],DF_edge_max['position'], '.', xp, p(xp), '-')
+
+plt.subplot(324)
+z = np.polyfit(DF_edge_max['time'], DF_edge_max['position'], 3)
+print(z)
+p = np.poly1d(z)
+print(p(10))
+xp = np.linspace(0, time_end, 1000)
+ax = plt.plot(DF_edge_max['time'],DF_edge_max['position'], '.', xp, p(xp), '-')
+
+plt.subplot(325)
+z = np.polyfit(DF_edge_max['time'], DF_edge_max['position'], 4)
+print(z)
+p = np.poly1d(z)
+print(p(10))
+xp = np.linspace(0, time_end, 1000)
+ax = plt.plot(DF_edge_max['time'],DF_edge_max['position'], '.', xp, p(xp), '-')
+
+plt.subplot(326)
+opt_parameters, pcov = opt.curve_fit(fitted_func, 
+				DF_edge_max[‘time’], 
+				DF_edge_max[‘position’])
+tp = np.linspace(0, time_end, 1000)
+ax = plt.plot(DF_edge_max['time'],DF_edge_max['position'], '.', 
+		tp, fitted_func(tp, *opt_parameters), '-')
+print(opt_parameters.tau)
+print(opt_parameters.a)
+
+DF_edge_max['time_scaled'] = DF_edge_max['time'] * float(t_step_size) 
+DF_edge_max['position_scaled'] = DF_edge_max['position'] * float(pixel_size) 
+time_end_scaled = time_end * float(t_step_size) 
+
+plt.figure()
+plt.subplot(121)
+z = np.polyfit(DF_edge_max['time_scaled'], DF_edge_max['position_scaled'], 3)
+print(z)
+p = np.poly1d(z)
+xp = np.linspace(0, time_end_scaled, 1000)
+ax = plt.plot(DF_edge_max['time_scaled'],DF_edge_max['position_scaled'], '.', xp, p(xp), '-')
+
+plt.subplot(122)
+p2 = np.polyder(p)
+print(p2)
+xpxp = np.linspace(0, time_end_scaled, 1000)
+ax = plt.plot(xpxp, p2(xpxp), '-')
+Derivative = p2(xpxp)
+print(Derivative)
+
+
+DF_edge_max['velocity'] = p2(DF_edge_max['time_scaled'])
+DF_v_t = pd.DataFrame({'timeframe': np.arange(1, time_end + 1 , 1),
+                      'actual_time': np.arange(float(t_step_size), time_end_scaled + float(t_step_size), float(t_step_size)),
+                      'velocity': p2(np.arange(float(t_step_size), time_end_scaled + float(t_step_size), float(t_step_size)))})
+
+os.chdir(path) 
+DF_v_t.to_excel('time_velocity.xlsx')   
+
+“””
+plt.figure()
+plt.subplot(121)
+real_opt_parameters, pcov = opt.curve_fit(fitted_func, 
+				DF_edge_max['time_scaled'], 								DF_edge_max['position_scaled'])
+tp = np.linspace(0, time_end_scaled, 1000);
+ax = plt.plot(DF_edge_max['time_scaled'],DF_edge_max['position_scaled'], '.',
+		tp, fitted_func(tp, *real_opt_parameters), ‘-‘)
+
+plt.subplot(122)
+ax = plt.plot(tp, derivative_fitted_func(tp, real_opt_parameters.b, 
+					real_opt_parameters.tau, ‘-‘))
+
+DF_edge_max['velocity'] = derivative_fitted_func(DF_edge_max['time_scaled’], 
+						real_opt_parameters.b, 
+						real_opt_parameters.tau)
+DF_v_t = pd.DataFrame({'timeframe': np.arange(1, time_end + 1 , 1),
+                      'actual_time': np.arange(float(t_step_size), time_end_scaled + float(t_step_size), float(t_step_size)),
+                      'velocity': derivative_fitted_func(np.arange(float(t_step_size), time_end_scaled + float(t_step_size), float(t_step_size)), real_opt_parameters.b, real_opt_parameters.tau)})
+
+os.chdir(path) 
+DF_v_t.to_excel('time_velocity.xlsx')   
+“””
+
+
+
+
+
+
---
+++ cellularization_1C.ijm
@@ -0,0 +1,102 @@
+// Cellularization analysis
+// Created by Yu-Chiun Wang
+
+fileName = getInfo("image.filename");
+path = getDirectory("image");
+
+//Creating a user interface using "Dialog"
+Dialog.create("File Identifier");
+Dialog.addString("Green Channel", "sqhGFP");
+Dialog.addString("Date/Identifier", "20150601");
+Dialog.addString("Serieal number", "01");
+Dialog.show();
+G_Ch	= Dialog.getString();
+Date_Id	= Dialog.getString();
+S_N 	= Dialog.getString();
+
+// Imaging parameters
+Dialog.create("Get frame rate");
+Dialog.addString("Frame rate (sec)", "2");
+Dialog.show();
+t_step = Dialog.getString();
+getPixelSize(unit, pixelWidth, pixelHeight);
+
+// Create folder
+newFolder = Date_Id + "_" + S_N + "_" + G_Ch;
+newPath = path + newFolder
+File.makeDirectory(newPath);
+
+//Combined channels
+selectWindow(fileName); 
+run("Duplicate...", "title=G1 duplicate channels=1");
+selectWindow(fileName); 
+run("Duplicate...", "title=G2 duplicate channels=2");
+imageCalculator("Add create stack", "G1","G2");
+run("Grays");
+Stack.getStatistics(area, mean, min, max);
+setMinAndMax(min, max);
+run("8-bit");
+newName = G_Ch + "_ch_fused";
+saveAs("tiff", newPath + "/" + newName);
+rename(newName);
+selectWindow("G1");
+close();
+selectWindow("G2");
+close();
+selectWindow(fileName); 
+close();
+
+// Preprocessing + make kymograph
+selectWindow(newName); 
+run("Gaussian Blur...", "sigma=3 stack"); // preprocessing
+waitForUser("Draw a straight segment across basal myosin (from top to bottom), decide on end point of analysis, then click OK");
+getLine(x1, y1, x2, y2, lineWidth);
+Dialog.create("Analysis end point");
+Dialog.addNumber("Final t frame", 0);
+Dialog.show();
+fin_t	= Dialog.getNumber();
+print(fin_t);
+
+selectWindow(newName); 
+run("Multi Kymograph", "linewidth=1");
+rename("raw kymograph");
+saveAs("tiff", newPath + File.separator + fileName + " kymograph raw");
+run("Gaussian Blur...", "sigma=3 stack"); // preprocess kymograph
+run("Duplicate...", "title=Kymo_seg");
+setAutoThreshold("Otsu dark");
+run("Make Binary");
+run("Outline");
+
+width = getWidth();
+height = getHeight();
+makeRectangle(0, 1, width, fin_t);
+run("Colors...", "foreground=black background=white selection=cyan");
+run("Clear Outside");
+run("Select None");
+waitForUser("Use magic wand to select basal margin, then click OK");
+run("Clear Outside");
+
+selectWindow("Kymograph");
+run("8-bit");
+run("Merge Channels...", "c1=Kymo_seg c4=Kymograph create keep ignore");
+saveAs("tiff", newPath + "/Merged");
+selectWindow("Kymo_seg");
+saveAs("tiff", newPath + "/cellu_front_y_t");
+close();
+selectWindow(newName);
+close();
+selectWindow("Kymograph");
+close();
+
+// Save some txt files
+print("\\Clear");
+print("t_step_size", "pixel_size");
+print(t_step, pixelHeight);
+selectWindow("Log");
+saveAs("txt", newPath + "/time_pixel_scales");
+
+print("\\Clear");
+print("Kymograph line segment coordinates:" + x1, y1, x2, y2);
+selectWindow("Log");
+saveAs("txt", newPath + "/Kymo_segment_cdn");
+
