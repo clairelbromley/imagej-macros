0d655c3185e457dce0cb373521c6247dedd5dc6e
douglas kelly <douglas.kelly@riken.jp>
2018-10-18 16:42:20 +0900

    Bring python implementation into line with where IJM implementation was, but with greater flexibility going forward

--- myosin_and_morphology.py
+++ myosin_and_morphology.py
@@ -19,6 +19,7 @@ from ij.io import OpenDialog, DirectoryChooser, FileSaver
 from ij.plugin import ChannelSplitter, Duplicator
 from ij.process import FloatPolygon, ImageConverter
 from ij.plugin.filter import GaussianBlur, ParticleAnalyzer
+from ij.plugin.frame import RoiManager
 from loci.plugins import BF as bf
 
 # searches for chosen file type within selected folder
@@ -44,12 +45,86 @@ def pause_for_debug():
 	gd.showDialog();
 	if gd.wasCanceled():
 		raise Exception("Run interupted");
-		
+
+def find_basal_edges(imp, search_range=10):
+	edges = [];
+	for fridx in range(0, imp.getNFrames()):
+		print("Examining frame " + str(fridx) + "...");
+		#imp.setPosition(1, 1, fridx+1);
+		imp.setSliceWithoutUpdate(fridx + 1)
+		if (fridx == 0):
+			edge = imp.getWidth() * [-1];
+			for xidx in range(0, imp.getWidth()):
+				yidx = imp.getHeight() - 1;
+				pix_val = imp.getPixel(xidx, yidx)[0];
+				while (pix_val == 0):
+					yidx = yidx - 1;
+					pix_val = imp.getPixel(xidx, yidx)[0];
+				edge[xidx] = yidx;
+		else:
+			# confine search for edge to region of search_range (10 pix default) either side
+			# of the last frame's edge. If no edge is found in that region, must be a hole in 
+			# the binary image. Assign a dummy value and deal with it in second loop. 
+			edge = imp.getWidth() * [-1];
+			for xidx in range(0, imp.getWidth()):
+				yidx = int(edges[fridx-1][xidx] + search_range);
+				pix_val = imp.getPixel(xidx, yidx)[0];
+
+				while (pix_val==0) and (yidx > (edges[fridx-1][xidx] - search_range)):
+					yidx = yidx - 1;
+					pix_val = imp.getPixel(xidx, yidx)[0];
+				if (pix_val != 0):
+					edge[xidx] = yidx;
+			
+			# deal with pixels we've marked as missing an edge
+			xidx = -1;
+			while xidx < imp.getWidth() - 1:
+				xidx = xidx + 1;
+				# if first position along edge is anomalous, take the value from the previous 
+				# frame's edge as a reasonable approximation
+				if (xidx==0) and (edge[xidx]==-1):
+					edge[xidx] = edges[fridx-1][xidx];
+					xidx = xidx + 1;
+				# if later positions are anomalous, interpolate between previous and next 
+				# successfully found edges
+				if (edge[xidx]==-1):
+					# last good edge position
+					x1 = xidx - 1;
+					y1 = edge[x1];
+					# find next good edge position
+					sub_xidx = xidx + 1;
+					wp = imp.getWidth() - 1;
+					if (sub_xidx < wp):
+						while (edge[sub_xidx]==-1):
+							sub_xidx = sub_xidx + 1;
+							if (sub_xidx >= wp):
+								break;
+					# deal with case when no further good positions exisit - just continue
+					# edge using last good y position to the end of the image in x direction, 
+					# and terminate outer loop over x positions
+					if (sub_xidx >= wp):
+						for subsub_xidx in range(x1, imp.getWidth()):
+							edge[subsub_xidx] = y1;
+						xidx = wp;
+					# deal with case when edge exists again after a while
+					else:
+						x2 = sub_xidx;
+						y2 = edge[x2];
+						# interpolate between last good position and next good position
+						for subsub_xidx in range((x1 + 1), x2):
+							edge[subsub_xidx] = round((float(subsub_xidx - x1)/
+															(x2 - x1)) * (y2 - y1) +  y1);
+						# get outer loop to skip over section we've just dealt with by updating
+						# xidx accordingly
+						xidx = x2 - 1;
+		edges.append(edge);	
+	return edges;
+
 def main():
 	#print (sys.version_info) # debug
 	#print(sys.path) # debug
-	data_root = r'C:\Users\Doug\Desktop\test'; # debug
-	output_root = r'C:\Users\Doug\Desktop\test'; #debug
+	data_root = r'C:\Users\dougk\Desktop\test'; # debug
+	output_root = r'C:\Users\dougk\Desktop\test'; #debug
 	#default_directory = r'C:\\Users\\Doug\\Desktop\\test';
 	#data_root, output_root = file_location_chooser(default_directory);
 	if (data_root is None) or (output_root is None):
@@ -118,16 +193,23 @@ def main():
 		IJ.run("Despeckle", "stack");
 		title = myo_imp.getTitle();
 
-		# perform size and position filtering on thresholded image...
-		IJ.run("Set Measurements...", "centroid stack redirect=None decimal=3");
-		IJ.run(myo_imp, "Analyze Particles...", "size=500-30000 pixel show=Masks display clear stack");
-		particles_imp = WindowManager.getImage(("Mask of " + title));
-		rt = ResultsTable.getResultsTable();
-		particle_slices = rt.getColumn(rt.getColumnsIndex("Slice"));
-		print(particle_slices)
-		frames = particles_imp.getNFrames();	
-		for fridx in range(0, frames):
-			imp.setSliceWithoutUpdate(fridx + 1);
+		# assume that first frame is good quality image...
+		basal_edges = find_basal_edges(myo_imp);
+		#myo_imp.hide()
+		mem_imp.hide();
+
+		# draw some edges for checking
+		roim = RoiManager();
+		xs = [x for x in range(1, trim_imp.getWidth()+1)];
+		trim_imp.show();
+		for fridx in range(0, myo_imp.getNFrames()):
+			trim_imp.setPosition(2, 1, fridx+1);
+			IJ.run("Enhance Contrast", "saturated=0.35");
+					
+			roi = PolygonRoi(xs, basal_edges[fridx], Roi.POLYLINE);
+			trim_imp.setRoi(roi);
+			roim.addRoi(roi);
+		#myo_imp.show()
 
 		# after getting rid of incorrectly segmented non-basal myosin, 
 		# perform previous edge search to identify the bottom AND TOP edges of the bright band
