4aaab3d01b52211666accbb9a9a6a1477b8864ff
douglas kelly <douglas.kelly@riken.jp>
2018-12-06 14:29:53 +0900

    Control actual scaling of data (normalised ACROSS THE STACK so that the maximum value of the stack is set to additional_scaling_factor * maximum for image type used, and minimum is pulled to zero) rather than just ImageJ's display

--- autosave_avis.py
+++ autosave_avis.py
@@ -2,12 +2,23 @@ import os
 from ij import IJ
 from ij.gui import WaitForUserDialog
 from ij.io import FileSaver
+from ij.process import StackStatistics;
 from loci.plugins import BF as bf
 
-# user parameters
+#----- user parameters -----
 root_folder = "C:\\Users\\dougk\\Desktop\\output";
 output_folder = "C:\\Users\\dougk\\Desktop\\avis";
 frame_rate_fps = 10;
+additional_scaling_factor = 0.5	# should be between 0 and 1
+#---------------------------
+
+def rescale_stack(imp, scaling):
+	"""Rescale whole stack so that contrast adjustment propagates to avi"""
+	stats = StackStatistics(imp);
+	rng = float(stats.max - stats.min);
+	IJ.run(imp, "Subtract...", "value=" + str(stats.min) + " stack");
+	IJ.run(imp, "Multiply...", "value=" + str(float(scaling * imp.getProcessor().maxValue())/rng) + " stack");
+	return imp;
 
 # make output folder if necessary
 if not os.path.isdir(output_folder):
@@ -26,7 +37,7 @@ for subfolder in subfolders:
 		bfimp = bf.openImagePlus(image_path);
 		imp = bfimp[0];
 		#imp.show();
-		IJ.run("Enhance Contrast", "saturated=0.35");
+		imp = rescale_stack(imp, additional_scaling_factor);
 		output_path = os.path.join(output_folder, (os.path.splitext(tiff_file)[0] + " autocontrast avi.avi"));
 		print("output_path = " + output_path);
 		IJ.run(imp, "AVI... ", "compression=None frame=" + str(frame_rate_fps) + " save=[" + output_path + "]");
