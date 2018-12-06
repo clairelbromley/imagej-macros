433de3675bb2dee66c1ce40563cbe24a875dad8f
douglas kelly <douglas.kelly@riken.jp>
2018-12-06 16:25:37 +0900

    Save as GIFs instead

--- autosave_avis.py
+++ autosave_avis.py
@@ -7,17 +7,28 @@ from loci.plugins import BF as bf
 
 #----- user parameters -----
 root_folder = "C:\\Users\\dougk\\Desktop\\output";
-output_folder = "C:\\Users\\dougk\\Desktop\\avis";
+output_folder = "C:\\Users\\dougk\\Desktop\\gifs";
 frame_rate_fps = 10;
 additional_scaling_factor = 0.5	# should be between 0 and 1
 #---------------------------
 
-def rescale_stack(imp, scaling):
+def rescale_stack(imp, scaling, subtract_minimum=True):
 	"""Rescale whole stack so that contrast adjustment propagates to avi"""
-	stats = StackStatistics(imp);
-	rng = float(stats.max - stats.min);
-	IJ.run(imp, "Subtract...", "value=" + str(stats.min) + " stack");
-	IJ.run(imp, "Multiply...", "value=" + str(float(scaling * imp.getProcessor().maxValue())/rng) + " stack");
+	if imp.getNSlices()>1:
+		stats = StackStatistics(imp);
+	else:
+		stats = imp.getStatistics();
+	if subtract_minimum:		
+		rng = float(stats.max - stats.min);
+		IJ.run(imp, "Subtract...", "value=" + str(stats.min) + " stack");
+	else:
+		rng = float(stats.max);
+	multiplier = float(scaling * imp.getProcessor().maxValue())/rng;
+	print("Multiplier = " + str(multiplier));
+	IJ.run(imp, "Multiply...", "value=" + str(multiplier) + " stack");
+	imp.show();
+	WaitForUserDialog("scaled image").show();
+	imp.hide();
 	return imp;
 
 # make output folder if necessary
@@ -36,10 +47,16 @@ for subfolder in subfolders:
 		print("input path = "  + image_path);
 		bfimp = bf.openImagePlus(image_path);
 		imp = bfimp[0];
+		IJ.run(imp, "8-bit", "");
 		#imp.show();
-		imp = rescale_stack(imp, additional_scaling_factor);
-		output_path = os.path.join(output_folder, (os.path.splitext(tiff_file)[0] + " autocontrast avi.avi"));
+		#imp = rescale_stack(imp, additional_scaling_factor, subtract_minimum=False);
+		IJ.run("Enhance Contrast", "saturated=0.35"); 
+		#WaitForUserDialog("Enhance!").show();
+		output_path = os.path.join(output_folder, (os.path.splitext(tiff_file)[0] + " autocontrast gif.gif"));
+		cal = imp.getCalibration();
+		cal.fps = frame_rate_fps;
+		imp.setCalibration(cal);
 		print("output_path = " + output_path);
-		IJ.run(imp, "AVI... ", "compression=None frame=" + str(frame_rate_fps) + " save=[" + output_path + "]");
+		IJ.saveAs(imp, "Gif", output_path);
 		imp.close();
 WaitForUserDialog("DONE!").show();
