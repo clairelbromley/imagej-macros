f81a6b94b033ecfcb5379544adbbf60223d0b85a
Claire Bromley <clairelbromley@gmail.com>
2018-12-08 12:22:27 +0900

    Tidying

--- autosave_avis.py
+++ autosave_avis.py
@@ -15,25 +15,6 @@ frame_rate_fps = 10; # output movie frame rate
 additional_scaling_factor = 0.3	# should be between 0 and 1
 #---------------------------
 
-def rescale_stack(imp, scaling, subtract_minimum=True):
-	"""Rescale whole stack so that contrast adjustment propagates to avi"""
-	if imp.getNSlices()>1:
-		stats = StackStatistics(imp);
-	else:
-		stats = imp.getStatistics();
-	if subtract_minimum:		
-		rng = float(stats.max - stats.min);
-		IJ.run(imp, "Subtract...", "value=" + str(stats.min) + " stack");
-	else:
-		rng = float(stats.max);
-	multiplier = float(scaling * imp.getProcessor().maxValue())/rng;
-	print("Multiplier = " + str(multiplier));
-	IJ.run(imp, "Multiply...", "value=" + str(multiplier) + " stack");
-	#imp.show();
-	#WaitForUserDialog("scaled image").show();
-	#imp.hide();
-	return imp;
-
 # make output folder if necessary
 if not os.path.isdir(output_folder):
 	os.makedirs(output_folder);
@@ -76,10 +57,7 @@ for subfolder in subfolders:
 				z_unit = "um";
 		
 		IJ.run(imp, "8-bit", "");
-		#imp.show();
-		#imp = rescale_stack(imp, additional_scaling_factor, subtract_minimum=True);
 		imp.show();
-		#WaitForUserDialog("prescale").show();
 		IJ.resetMinAndMax();
 		IJ.run("Enhance Contrast", "saturated=0.35");
 		if imp.getNFrames() > imp.getNSlices():
@@ -90,14 +68,9 @@ for subfolder in subfolders:
 			IJ.run(imp, "Label...", "format=0 starting=0 interval=" + str(z_interval) + 
 					" x=5 y=20 font=18 text=[ " + str(z_unit)  + 
 					"] range=1-" + str(imp.getNSlices()) + " use_text");
-		#WaitForUserDialog("postscale, postlabel").show();
 		output_path = os.path.join(output_folder, (os.path.splitext(tiff_file)[0] + " autocontrast avi.avi"));
 		IJ.run(imp, "AVI... ", "compression=None frame=" + str(frame_rate_fps) + " save=[" + output_path + "]");
-		#cal = imp.getCalibration();
-		#cal.fps = frame_rate_fps;
-		#imp.setCalibration(cal);
 		print("output_path = " + output_path);
-		#IJ.saveAs(imp, "Gif", output_path);
 		imp.changes=False;
 		imp.close();
 WaitForUserDialog("DONE!").show();
