e28c1a66055923971a7bf45a65cbae67e29957f6
Doug Kelly <dougkelly88@googlemail.com>
2018-12-10 21:07:05 +0900

    Use main function

--- overlay_line_scan.py
+++ overlay_line_scan.py
@@ -43,7 +43,7 @@ class LineScanOverlayMetadata:
 			loresp1y = int(round(self.p1[1]/factor)) - self.referenceRoiOffsetXY[1];
 			loresp2x = int(round(self.p2[0]/factor)) - self.referenceRoiOffsetXY[0];
 			loresp2y = int(round(self.p2[1]/factor)) - self.referenceRoiOffsetXY[1];
-			print([(loresp1x, loresp1y), (loresp2x,loresp2y)]);
+			#print([(loresp1x, loresp1y), (loresp2x,loresp2y)]);
 			return Line(loresp1x, loresp1y, loresp2x, loresp2y);
 		else:
 			return Line(self.p1[0] - self.referenceRoiOffsetXY[0], 
@@ -91,45 +91,51 @@ class LineScanOverlayMetadata:
 		return True;
 
 
-dialog = GenericDialog("Define 5x ROI parameters...");
-dialog.addNumericField("Line scan position 1 X, Y: ", 0, 0);
-dialog.addToSameRow();
-dialog.addNumericField(", ", 0, 0);
-dialog.addNumericField("Line scan position 2 X, Y: ", 0, 0);
-dialog.addToSameRow();
-dialog.addNumericField(", ", 0, 0);
-dialog.addNumericField("Reference image crop origin X, Y: ", 0, 0);
-dialog.addToSameRow();
-dialog.addNumericField(", ", 0, 0);
-dialog.addNumericField("Frame interval (s): ", 1.0, 2);
+def main():
+	dialog = GenericDialog("Define 5x ROI parameters...");
+	dialog.addNumericField("Line scan position 1 X, Y: ", 0, 0);
+	dialog.addToSameRow();
+	dialog.addNumericField(", ", 0, 0);
+	dialog.addNumericField("Line scan position 2 X, Y: ", 0, 0);
+	dialog.addToSameRow();
+	dialog.addNumericField(", ", 0, 0);
+	dialog.addNumericField("Reference image crop origin X, Y: ", 0, 0);
+	dialog.addToSameRow();
+	dialog.addNumericField(", ", 0, 0);
+	dialog.addNumericField("Frame interval (s): ", 1.0, 2);
+	
+	dialog.showDialog();
+	
+	p1x = dialog.getNextNumber();
+	p1y = dialog.getNextNumber();
+	p2x = dialog.getNextNumber();
+	p2y = dialog.getNextNumber();
+	offsetx = dialog.getNextNumber();
+	offsety = dialog.getNextNumber();
+	interval = dialog.getNextNumber();
+	linescan_overlay = LineScanOverlayMetadata((p1x,p1y), (p2x,p2y), (offsetx,offsety), interval);
+	#print("p1 = " + str(linescan_overlay.p1));
+	#print("p2 = " + str(linescan_overlay.p2));
+	
+	roi = linescan_overlay.generateOverlayRoi();
+	#print(roi);
+	imp.setRoi(roi);
+	IJ.run(imp, "RGB Color", "");
+	IJ.run("Colors...", "foreground=cyan background=white selection=yellow");
+	IJ.run("Draw", "stack");
+	
+	sd = SaveDialog("Save as AVI...", os.path.splitext(imp.getTitle())[0], "line scan overlay.avi");
+	if sd.getFileName is not None:
+		linescan_overlay.save_to_json(os.path.join(sd.getDirectory(), 
+													   os.path.splitext(os.path.basename(sd.getFileName()))[0] + "line scan metadata.json"));
+		IJ.saveAs(imp, "Tiff", os.path.join(sd.getDirectory(), 
+				os.path.splitext(os.path.basename(sd.getFileName()))[0] + " + line scan ROI, no timestamp.tif"));
+		IJ.run(imp, "Label...", "format=00:00:00 starting=0 interval=" + str(interval) + " x=5 y=20 font=18 text=[] range=1-"+str(imp.getNFrames()));
+		IJ.run(imp, "AVI... ", "compression=None frame=10 save=[" + os.path.join(sd.getDirectory(), (os.path.splitext(sd.getFileName())[0] + " + line scan ROI.avi")) + "]");
+		IJ.saveAs(imp, "Tiff", os.path.join(sd.getDirectory(), 
+				os.path.splitext(os.path.basename(sd.getFileName()))[0] + " + line scan ROI.tif"));
 
-dialog.showDialog();
-
-p1x = dialog.getNextNumber();
-p1y = dialog.getNextNumber();
-p2x = dialog.getNextNumber();
-p2y = dialog.getNextNumber();
-offsetx = dialog.getNextNumber();
-offsety = dialog.getNextNumber();
-interval = dialog.getNextNumber();
-linescan_overlay = LineScanOverlayMetadata((p1x,p1y), (p2x,p2y), (offsetx,offsety), interval);
-print("p1 = " + str(linescan_overlay.p1));
-print("p2 = " + str(linescan_overlay.p2));
-
-roi = linescan_overlay.generateOverlayRoi();
-print(roi);
-imp.setRoi(roi);
-IJ.run(imp, "RGB Color", "");
-IJ.run("Colors...", "foreground=cyan background=white selection=yellow");
-IJ.run("Draw", "stack");
-
-sd = SaveDialog("Save as AVI...", os.path.splitext(imp.getTitle())[0], "line scan overlay.avi");
-if sd.getFileName is not None:
-	linescan_overlay.save_to_json(os.path.join(sd.getDirectory(), 
-												   os.path.splitext(os.path.basename(sd.getFileName()))[0] + "line scan metadata.json"));
-	IJ.saveAs(imp, "Tiff", os.path.join(sd.getDirectory(), 
-			os.path.splitext(os.path.basename(sd.getFileName()))[0] + " + line scan ROI, no timestamp.tif"));
-	IJ.run(imp, "Label...", "format=00:00:00 starting=0 interval=" + str(interval) + " x=5 y=20 font=18 text=[] range=1-"+str(imp.getNFrames()));
-	IJ.run(imp, "AVI... ", "compression=None frame=10 save=[" + os.path.join(sd.getDirectory(), (os.path.splitext(sd.getFileName())[0] + " + line scan ROI.avi")) + "]");
-	IJ.saveAs(imp, "Tiff", os.path.join(sd.getDirectory(), 
-			os.path.splitext(os.path.basename(sd.getFileName()))[0] + " + line scan ROI.tif"));
+# It's best practice to create a function that contains the code that is executed when running the script.
+# This enables us to stop the script by just calling return.
+if __name__ in ['__builtin__','__main__']:
+    main()
